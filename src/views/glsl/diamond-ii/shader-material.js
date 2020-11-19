180451811601a55cbb19f11ddaeb1d259af1da91
nelsonkuang <410374308.qq.com>
2020-11-19 19:28:36 +0800

    调试

--- src/views/glsl/diamond-ii/shader-material.js
+++ src/views/glsl/diamond-ii/shader-material.js
@@ -3,6 +3,8 @@ import frontMaterial_vert from './front-material-vs.glsl'
 import frontMaterial_frag from './front-material-fs.glsl'
 import backMaterial_vert from './back-material-vs.glsl'
 import backMaterial_frag from './back-material-fs.glsl'
+import dp2_vert from './dp2.v.glsl'
+import dp2_frag from './dp2.f.glsl'
 
 export const getFrontMaterial = function (THREE, cubeTexture, refractTexture) {
     return new THREE.ShaderMaterial({
@@ -44,6 +46,39 @@ export const getBackMaterial = function (THREE, cubeTexture, refractTexture) {
             "backAlpha": { value: 0.5 },
             "frontAlpha": { value: 0.5 },
             "reflectionStrength": { value: 2.0 },
+        },
+    })
+}
+
+export const getDpMaterial = function (THREE, cubeMap, envMap, side) {
+    return new THREE.RawShaderMaterial({
+        vertexShader: dp2_vert,
+        fragmentShader: dp2_frag,
+        side: side,
+        transparent: true,
+        depthWrite: false,
+        uniforms: {
+            'modelMatrix': { value: new THREE.Matrix4() },
+            'InverseModelMatrix': { value: new THREE.Matrix4() },
+            'modelViewMatrix': { value: new THREE.Matrix4() },
+            'projectionMatrix': { value: new THREE.Matrix4() },
+            'cameraPosition': { value: [0, 0, 0] },
+            'tCubeMapNormals': { type: 't', value: cubeMap },
+            'envMap': { type: 't', value: envMap },
+            'envMapIntensity': { value: 1.5 },
+            'n2': { value: 0.1 },
+            'radius': { value: 1.0 },
+            'rIndexDelta': { value: 1.0 },
+            'normalOffset': { value: 0.1 },
+            'squashFactor': { value: 0.1 },
+            'distanceOffset': { value: 0.1 },
+            'geometryFactor': { value: 2.0 },
+            'colorCorrection': { value: [1, 1, 1] },
+            'boostFactors': { value: [1, 1, 1] },
+            'centreOffset': { value: [1, 1, 1] },
+            'opacity': { value: 0.5 },
+            'i_toneMappingExposure': { value: 1.0 },
+            'i_toneMappingWhitePoint': { value: 2.0 }
         },
     })
 }
